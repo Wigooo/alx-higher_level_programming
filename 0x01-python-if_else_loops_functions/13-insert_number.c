e4c54ae812f5676dd49ccf866e0211e737b75056
Mohamed <abdoashraf973@gmial.com>
2023-10-31 17:05:23 -0700

    updated the wrong tasks

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -15,7 +15,7 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *node = *head, *new = malloc(sizeof(listint_t));
 
 	if (!new)
-		return(NULL);
+		return (NULL);
 
 	new->n = number;
 	new->next = NULL;
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(10):
-    for x in range(i, 10):
-        if i < x:
-            print("{:d}{:d}".format(i, x),
-                end="\n" if i == 0 and x == 9 else ", ")
+    for j in range(i, 10):
+        if i < j:
+            print("{:d}{:d}".format(i, j),
+                end="\n" if i == 8 and j == 9 else ", ")
